e2362de75cfb28eb820444a49d1a6df57eae0a95
Michelle Pai <michelle.pai@sentaca.com>
2013-09-25 16:30:12 -0700

    add and remove operation and operation detail form the same time. Remove 'Insert operation detail" button.

--- js/js.js
+++ js/js.js
@@ -1,15 +1,43 @@
 $(document).ready(function() {
     var ls = window.localStorage;
-
+    $('.hide').css('display', 'none');
+    
+    //need to set opCount sync with the localstorage
+    
     if (ls.getItem('1opsCount') !== null) {
         opsCount = ls.getItem('1opsCount');
-        for (i = 0; i < (opsCount - 1); i++) {
-            $('#op_summary tr').eq(1).clone().find('input').val('').end()
+        for (i = 1; i <= opsCount; i++) {
+            if(i!==1){
+                $('#op_summary tr').eq(1).clone().find('input').val('').end()
                     .appendTo('#op_summary')
                     .find("*[name]")
                     .each(function() {
-                $(this).attr("name", $(this).attr("name").replace(($(this).attr("name").charAt(4)), (i + 2)));
+                $(this).attr("name", $(this).attr("name").replace(($(this).attr("name").charAt(4)),i));
             });
+            }
+            
+            cloneDiv = $('#default_operation').clone();
+            cloneDiv.appendTo('#input_form')
+                    .prepend('<h2>' + ls.getItem('spec_form/spec_form:ops' + i + '_name') + '</h2>')
+                    .css('display', 'block')
+                    .attr('id', 'operation_' + i)
+                    .sisyphus({timeout: 5});
+            cloneDiv.find('*[name]').each(function() {
+                var new_name = $(this).attr("name").slice(0, 4) + i + $(this).attr("name").slice(5);
+                this.name = new_name;
+                this.id = new_name;
+                console.log('name: ' + this.name + ' id: ' + this.id);
+            });
+            cloneDiv.find(':button.btn_clone').each(function() {
+                var new_id = $(this).attr("id").slice(0, 6) + i + $(this).attr("id").slice(7);
+                this.id = new_id;
+            });
+            cloneDiv.find('table.tbl_clone').each(function() {
+                var new_id = $(this).attr("id").slice(0, 6) + i + $(this).attr("id").slice(7);
+                this.id = new_id;
+
+            });
+                               
         }
     }else{
         opsCount=1;
@@ -22,16 +50,40 @@ $(document).ready(function() {
                 .each(function() {
             $(this).attr("name", $(this).attr("name").replace(($(this).attr("name").charAt(4)), opsCount));
         });
+        cloneDiv = $('#default_operation').clone();
+            cloneDiv.appendTo('#input_form')
+                    .prepend('<h2>' + ls.getItem('spec_form/spec_form:ops' + i + '_name') + '</h2>')
+                    .css('display', 'block')
+                    .attr('id', 'operation_' + i)
+                    .sisyphus({timeout: 5});
+            cloneDiv.find('*[name]').each(function() {
+                var new_name = $(this).attr("name").slice(0, 4) + i + $(this).attr("name").slice(5);
+                this.name = new_name;
+                this.id = new_name;
+                console.log('name: ' + this.name + ' id: ' + this.id);
+            });
+            cloneDiv.find(':button.btn_clone').each(function() {
+                var new_id = $(this).attr("id").slice(0, 6) + i + $(this).attr("id").slice(7);
+                this.id = new_id;
+            });
+            cloneDiv.find('table.tbl_clone').each(function() {
+                var new_id = $(this).attr("id").slice(0, 6) + i + $(this).attr("id").slice(7);
+                this.id = new_id;
+
+            });
         ls.setItem('1opsCount', opsCount);
 
     });
     $('#rm_op').click(function() {
         opsCount--;
         $('#op_summary tr').last('tr').remove();
+        divToRemove = 'operation_'+(opsCount+1);
+        $('#'+divToRemove).remove();
         ls.setItem('1opsCount', opsCount);
+        
     });
 
-    $('.hide').css('display', 'none');
+    
     $('#gen_form').click(function() {
         for (var i = 1; i <= opsCount; i++) {
             cloneDiv = $('#default_operation').clone();
